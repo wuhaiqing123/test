7ea827fae88e249bcd6ca6a26a2024a03e20e5b7
wuhaiqing123 <269844098@qq.com>
2018-10-06 20:24:20 +0800

    第一次提交

--- src/main/java/com/imooc/config/ConfigApplication.java
+++ src/main/java/com/imooc/config/ConfigApplication.java
@@ -7,7 +7,6 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableConfigServer
 public class ConfigApplication {
 
     public static void main(String[] args) {
