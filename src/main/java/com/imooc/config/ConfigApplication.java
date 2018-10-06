2db802f1c037e18afcf7446aaa490ad2eb4102c6
wuhaiqing123 <269844098@qq.com>
2018-10-06 20:25:49 +0800

    测试代码

--- src/main/java/com/imooc/config/ConfigApplication.java
+++ src/main/java/com/imooc/config/ConfigApplication.java
@@ -7,6 +7,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableConfigServer
 public class ConfigApplication {
 
     public static void main(String[] args) {
